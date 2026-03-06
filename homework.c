59058fc3d606bfcd2115bc77e13830abf24540d1
karl-code-2006 <261086483@qq.com>
2026-03-06 15:46:33 +0800

    Add files via upload

--- homework.c
+++ homework.c
@@ -1,47 +1,25 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-struct student {
-    int id;
-    char name[20];
-    int age;
-};
-
-int compareByName(const void *a, const void *b) {
-    struct student *s1 = (struct student*)a;
-    struct student *s2 = (struct student*)b;
-    return strcmp(s1->name, s2->name);
+int main() {
+	char letter;
+	char word[201];
+	
+	scanf(" %c %s", &letter, word);
+	
+	letter = tolower(letter);
+	int length = strlen(word);
+	int count = 0;
+	
+	for (int i = 0; i < length; i++) {
+		if (tolower(word[i]) == letter) {
+			count++;
+		}
+	}
+	
+	double probability = (double)count / length;
+	printf("%.5f\n", probability);
+	
+	return 0;
 }
-
-int compareByAge(const void *a, const void *b) {
-    struct student *s1 = (struct student*)a;
-    struct student *s2 = (struct student*)b;
-    if(s1->age != s2->age) return s1->age - s2->age;
-    return strcmp(s1->name, s2->name);
-}
-
-int main(void) {
-    struct student students[50];
-    int n;
-    scanf("%d", &n);
-    
-    for(int i = 0; i < n; i++) {
-        scanf("%d %s %d", &students[i].id, students[i].name, &students[i].age);
-    }
-    
-    qsort(students, n, sizeof(struct student), compareByName);
-    for(int i = 0; i < n; i++) {
-        printf("%3d%6s%3d\n", students[i].id, students[i].name, students[i].age);
-    }
-    
-    printf("\n");
-    
-    qsort(students, n, sizeof(struct student), compareByAge);
-    for(int i = 0; i < n; i++) {
-        printf("%3d%6s%3d\n", students[i].id, students[i].name, students[i].age);
-    }
-    
-    return 0;
-}
-
